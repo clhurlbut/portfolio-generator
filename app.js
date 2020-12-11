86692f7315c6166d561d3607528bfb972bcf7c90
CassBut <clhurlbut@gmail.com>
2020-12-10 18:15:06 -0800

    create html file from the command line

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2);
+/* const name = profileDataArgs[0];
+const github = profileDataArgs[1]; */
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+/* const printProfileData = profileDataArr => {
     // This...
     for (let i = 0; i < profileDataArr.length; i += 1) {
         console.log(profileDataArr[i]);
@@ -12,4 +25,4 @@ const printProfileData = profileDataArr => {
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs); */
